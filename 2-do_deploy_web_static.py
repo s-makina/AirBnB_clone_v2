e779e896f8fa151875c71dc3bf650377f829055b
Salvation Makina <salvation.developer@gmail.com>
2023-04-06 19:19:48 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['54.157.128.248', '100.25.37.224']
 
 
 def do_deploy(archive_path):
